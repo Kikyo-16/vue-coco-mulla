c2a0c3c05e52997aef16313f8c4c61423649a71a
Kikyo-16 <1174436431@qq.com>
2023-10-26 22:35:24 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,17 @@
   </div>
   <div class="card">
     <div class="sub-sec">
-        We equip <a href="https://arxiv.org/abs/2306.05284">MusicGen</a>, a text-to-music generation model, with direct and <b>content-based controls</b> on innate music languages,
-        including chords, rhythms, and pianoroll.
-        To achieve this, we contribute a Parameter-Efficient Fine-Tuning (PEFT) method tailored for
-        Transformer-based audio language model fine-tuning. Our approach achieved
-        low-resource semi-supervised learning, tuning with less than 4% parameters compared
+        We equip <a href="https://arxiv.org/abs/2306.05284">MusicGen</a>, a text-to-music generation model,  with direct and content-based controls on innate music
+languages such as pitch, chords and drum track.
+        To this end, we contribute Coco-Mulla, a <b>co</b>ntent-based <b>co</b>ntrol method for <b>mu</b>sic <b>l</b>arge <b>la</b>nguage modeling. It uses
+a parameter-efficient fine-tuning (PEFT) method tailored for Transformer-based
+audio models. Our approach achieved low-resource semi-supervised learning, tuning with less than 4% parameters compared
         to the original model and training on a small dataset set with fewer than 300 <i>unannotated</i> songs.
-        We illustrate the <b>chords</b> and <b>rhythms</b>  control power of the model. Moreover, by combining pianoroll and text descriptions,
-        our system enables flexible music <b>style transfer</b> and <b>music variation generation</b>.
+        We illustrate the <b>chords</b> and <b>rhythms</b>  control power of the model. Moreover, by combining <b>piano roll</b> and text descriptions,
+        our system enables flexible music variation generation and style transfer.
       <br>
       <br>
-      See more details in our paper [coming soon] and try our model via github [coming soon] or huggingface [coming soon].
+      See more details in our <a href="https://arxiv.org/pdf/2310.17162.pdf">paper</a> and try our model via github [coming soon] or huggingface [coming soon].
 
     </div>
   </div>
